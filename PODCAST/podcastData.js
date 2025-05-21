3b3942d84f26adf81b55b3e133764223fc93c32c
kant799 <33680685+kant799@users.noreply.github.com>
2025-05-21 11:37:52 +0800

    Add files via upload

---
+++ PODCAST/podcastData.js
@@ -0,0 +1,220 @@
+// 提供从上一步任务获取的原始JSON数据
+
+// 根据ID获取对应的原始JSON数据
+function getOriginalJsonById(id) {
+    switch (id) {
+        case '681dc93cb7c8a9962c3fb0e6': // 不开玩笑
+            return `{
+  "pageProps": {
+    "episode": {
+      "_id": "681dc93cb7c8a9962c3fb0e6",
+      "title": "和西班牙语从相忘走到相知",
+      "subtitle": "所以我是如何发自内心地爱上西班牙语的？",
+      "contentType": "free",
+      "status": "published",
+      "podcasterInfo": {
+        "_id": "5e280d223e59d20001e0f1ac",
+        "nickname": "Xiaoyu"
+      },
+      "podcast": {
+        "_id": "5e280d223e59d20001e0f1ab",
+        "title": "不开玩笑",
+        "subtitle": "大家好，我是 Xiaoyu。不开玩笑聊点正经话题。不随便更新，但都是自己喜欢的内容。",
+        "groupMembers": [
+          "5e280d223e59d20001e0f1ac"
+        ],
+        "subscriptionCount": 112435,
+        "cover": "//imagev2.xmcdn.com/storages/1e53-audiofreehighqps/40/12/GMCoOSIFbDCqAAAE0AEP9mWC.jpg!strip=0!quality=7"
+      },
+      "shownotes": "所以我是如何发自内心地爱上西班牙语的？\n\n想要收听会员内容，比如跟我学西班牙语系列，欢迎到「小宇宙 app 」开通会员，感谢支持。\n\n00:03 开场与更新\n06:00 西语学习的开始——墨西哥小镇\n12:40 两个学习阶段不同的心态\n20:08 学习的策略\n26:07 今天中文播客推荐\n28:42 结语\n\n有任何想法或反馈欢迎微信联系 BKWJpod，或者邮件 bukaiwan.joke@gmail.com\n\n在这一集中推荐的内容：\n\n语言类播客《语言习得》\nhttps://www.xiaoyuzhoufm.com/podcast/611719ec9cbf5eca72d9b80c\n\n电影《罗马》\nhttps://movie.douban.com/subject/1950330/\n\n剧集《毒枭》\nhttps://movie.douban.com/subject/25926859/\n\nFluent Spanish Academy\nhttps://www.fluentspanishacademy.com",
+      "publicationDate": "2023-04-03T11:37:12+08:00",
+      "duration": "00:29:52",
+      "audioUrl": "//jt.ximalaya.com//GKwRITkFdZwAABUABgC9WX6Z.m4a?_wv=1212113f0c4c5b9e37f5e1e0a1c58deb&channel=rss&download=1&player_type=1&share_count=0&sign=c0cd0c8f4d5f983d0fc4d18582e4c1d0&ts=1629945531&user_id="
+    },
+    "__N_SSP": true
+  },
+  "page": "/episode/[id]",
+  "query": {
+    "id": "681dc93cb7c8a9962c3fb0e6"
+  },
+  "buildId": "ukNZu6IpYNggYjnOGPzVW",
+  "assetPrefix": "",
+  "isFallback": false,
+  "gssp": true,
+  "customServer": true,
+  "scriptLoader": []
+}`;
+  
+        case '682a0676a5412dad8cd29bcc': // 无聊斋
+            return `{
+  "pageProps": {
+    "episode": {
+      "_id": "682a0676a5412dad8cd29bcc",
+      "title": "E55|不聊技术的《驯龙高手》",
+      "subtitle": "有些技术需要比较高的制作人、导演水平，才能做到不让观众觉得技术感太强。《驯龙高手》就很好的包裹了它的技术，所以你能更强的沉浸到一个正经的故事里，这很不容易。",
+      "contentType": "free",
+      "status": "published",
+      "podcasterInfo": {
+        "_id": "611a7f45ba79fa2fe2bdb0dd",
+        "nickname": "娜娜"
+      },
+      "podcast": {
+        "_id": "611eb81be0a3eb6959b14ee9",
+        "title": "无聊斋",
+        "subtitle": "娜娜的两厢情愿。",
+        "groupMembers": [
+          "611a7f45ba79fa2fe2bdb0dd"
+        ],
+        "subscriptionCount": 30489,
+        "cover": "//imagev2.xmcdn.com/storages/1a40-audiofreehighqps/36/0C/GMCoOR8GsQU6AAGp8AEMQW4p.jpg"
+      },
+      "shownotes": "有些技术需要比较高的制作人、导演水平，才能做到不让观众觉得技术感太强。《驯龙高手》就很好的包裹了它的技术，所以你能更强的沉浸到一个正经的故事里，这很不容易。一个不是太技术流的影片，有更强的互动感。\n\n简介：\n本节目致力于探讨生活的边边角角。我会分享生活中作为普通人的灵感、疑惑和想法... 为闲暇时刻带来陪伴。\n\n相关频道：\nInstagram：https://www.instagram.com/nanayo_wuliangzhai/\nSpotify：https://open.spotify.com/show/4BcZUq1tgOUZm96blJ5g42?si=f7bc26010bca486f\n\n文末图片：\n新年祝福\n\n相关内容：\n《驯龙高手》",
+      "publicationDate": "2023-04-13T20:01:42+08:00",
+      "duration": "00:38:58",
+      "audioUrl": "//jt.ximalaya.com//GKwRIaMFmucQABmQABsBM2zq.m4a?channel=rss&download=1&player_type=1&share_count=0&sign=5fcbf4dfb3c2c4de7b1a87fb069b8fba&ts=1682417639&user_id="
+    },
+    "__N_SSP": true
+  },
+  "page": "/episode/[id]",
+  "query": {
+    "id": "682a0676a5412dad8cd29bcc"
+  },
+  "buildId": "ukNZu6IpYNggYjnOGPzVW",
+  "assetPrefix": "",
+  "isFallback": false,
+  "gssp": true,
+  "customServer": true,
+  "scriptLoader": []
+}`;
+            
+        case '6828c15d457b22ce0d318955': // 基本无害
+            return `{
+  "pageProps": {
+    "episode": {
+      "_id": "6828c15d457b22ce0d318955",
+      "title": "E142丨读《呼兰河传》：一个丰富的关于贫穷的故事",
+      "subtitle": "《呼兰河传》太值得读了，我很推荐大家读。萧红写得实在好，是真正的有用的自传文学。",
+      "contentType": "free",
+      "status": "published",
+      "podcasterInfo": {
+        "_id": "5fab42fb72bdc2f6b0a7a92e",
+        "nickname": "NABRA"
+      },
+      "podcast": {
+        "_id": "60d01f78bf0ca14e43d8add6",
+        "title": "基本无害",
+        "subtitle": "一档关注世界上各种各样事物的节目。将世界的复杂性和不确定性作为首要追求，一次一个主题，尽量基本无害。",
+        "groupMembers": [
+          "5fab42fb72bdc2f6b0a7a92e"
+        ],
+        "subscriptionCount": 22842,
+        "cover": "//imagev2.xmcdn.com/storages/06f1-audiofreehighqps/9C/D5/CMCoOR4Dm7G5AAGb9wDpDIzP.jpeg"
+      },
+      "shownotes": "《呼兰河传》太值得读了，我很推荐大家读。萧红写得实在好，是真正的有用的自传文学。\n\n在台风来的夜晚录制的一期节目。\n周末我们要飞，在下周我或许会发一些关于飞行的内容。\n\n开头的歌是 Marianne Faithfull 的 Young Girl Blues。\n结尾的歌来自程璧的《给青年恋人们》，原曲是谷村新司的《いい日旅立ち》，翻唱是1976年的南方二重唱。\n\n我的播客梦想是跟上播客圈。\n\n本期提到了：\n萧红《呼兰河传》\n关于萧红的小说《黄金时代》\n\n节目官网：jibenwuhai.com\n\n邮件：nabra@jibenwuhai.com\n\n正经网站：nabra.me",
+      "publicationDate": "2023-04-12T11:48:30+08:00",
+      "duration": "00:43:15",
+      "audioUrl": "//jt.ximalaya.com//GKwRIaIFl8M6AAMBBwDPj3LV.m4a?_wv=1aa39dc7ff&amp;channel=rss&amp;download=1&amp;player_type=1&amp;sign=c05dcf8fd32ce62bff8e08ee7d5ad66d&amp;ts=1681398536&amp;user_id="
+    },
+    "__N_SSP": true
+  },
+  "page": "/episode/[id]",
+  "query": {
+    "id": "6828c15d457b22ce0d318955"
+  },
+  "buildId": "ukNZu6IpYNggYjnOGPzVW",
+  "assetPrefix": "",
+  "isFallback": false,
+  "gssp": true,
+  "customServer": true,
+  "scriptLoader": []
+}`;
+        
+        case '6824bac11ced30a23159f50b': // 三个火呛手
+            return `{
+  "pageProps": {
+    "episode": {
+      "_id": "6824bac11ced30a23159f50b",
+      "title": "Vol.07 日本的街头流浪歌手真的很浪漫吗？",
+      "subtitle": "前五集是朋友给的建议，先去聊我们自己感兴趣的话题，从第六期开始，我们会更加关注大家所感兴趣的话题，欢迎大家继续关注。",
+      "contentType": "free",
+      "status": "published",
+      "podcasterInfo": {
+        "_id": "65bb77b5a5412dad8cc5c86c",
+        "nickname": "大家"
+      },
+      "podcast": {
+        "_id": "65cf70a5a5412dad8cdccce3",
+        "title": "三个火呛手",
+        "subtitle": "三个关注小众音乐的音乐爱好者火呛的日常",
+        "groupMembers": [
+          "65bb77b5a5412dad8cc5c86c"
+        ],
+        "subscriptionCount": 3210,
+        "cover": "//imagev2.xmcdn.com/storages/10a6-audiofreehighqps/A1/E4/GKwRIaIFSUYaAATnMQEyFAo7.jpg"
+      },
+      "shownotes": "前五集是朋友给的建议，先去聊我们自己感兴趣的话题，从第六期开始，我们会更加关注大家所感兴趣的话题，欢迎大家继续关注。\n\n本期内容：\n1、国内的街头流浪歌手现状\n2、日本的街头流浪歌手现状\n3、有哪些日本的流浪歌手值得关注？\n\n本期嘉宾：刘聪\n\n朋友们可以与我们互动的平台：\nDOUYIN：三个火呛手\n小红书：三个火呛手\n邮件：sangehuoqiangshou@gmail.com\n\n一键进入我们建立的歌单：\n\nhttps://open.spotify.com/user/31sj6fudyotquhzzcfdw5wf325wi?si=Ut5I0A3hSD2Q0C9EBXXFcg&nd=1&dlsi=96b9a6a31baa478b \n\n希望有共同想法的朋友们联系我们",
+      "publicationDate": "2023-04-09T23:00:00+08:00",
+      "duration": "01:01:40",
+      "audioUrl": "//jt.ximalaya.com//GKwRITsFh8oZAAZnqQFXARAP.m4a?channel=rss&download=1&player_type=1&share_count=0&sign=ded0f6f0fab7c17eb49cf55a22e62ad9&ts=1681399225&user_id="
+    },
+    "__N_SSP": true
+  },
+  "page": "/episode/[id]",
+  "query": {
+    "id": "6824bac11ced30a23159f50b"
+  },
+  "buildId": "ukNZu6IpYNggYjnOGPzVW",
+  "assetPrefix": "",
+  "isFallback": false,
+  "gssp": true,
+  "customServer": true,
+  "scriptLoader": []
+}`;
+
+        case '682b4b04457b22ce0d807186': // 正经叭叭
+            return `{
+  "pageProps": {
+    "episode": {
+      "_id": "682b4b04457b22ce0d807186",
+      "title": "16. 小狗肉夹馍（下）｜遗憾不止小狗",
+      "subtitle": "主播遗憾的，可能是给大家带来了一个沮丧的故事。生活还是要继续的呀~",
+      "contentType": "free",
+      "status": "published",
+      "podcasterInfo": {
+        "_id": "5feea1e9bacc851aaa6bdb8a",
+        "nickname": "晓晓小灰灰"
+      },
+      "podcast": {
+        "_id": "620aa8c15795d7dbfb06cb77",
+        "title": "正经叭叭",
+        "subtitle": "一档来自普通人的随笔记，探讨有趣的人和事，关注生活的点滴。",
+        "groupMembers": [
+          "5feea1e9bacc851aaa6bdb8a"
+        ],
+        "subscriptionCount": 3095,
+        "cover": "//imagev2.xmcdn.com/storages/ffcf-audiofreehighqps/7C/FE/GKwRIUIGQQK5AB-JogEC4Vzv.jpg!strip=0!quality=7"
+      },
+      "shownotes": "主播遗憾的，可能是给大家带来了一个沮丧的故事。生活还是要继续的呀~\n\n微信公众号：正经叭叭 podcast\n小红书：正经叭叭\nInstagram：zhengjingbaba\n\n如果觉得有收获，欢迎给节目五星好评、留言推荐给朋友！！！",
+      "publicationDate": "2023-04-14T15:08:26+08:00",
+      "duration": "00:52:43",
+      "audioUrl": "//jt.ximalaya.com//GKwRIaoFnHBZAASwugHW3Gn_.m4a?channel=rss&download=1&player_type=1&share_count=0&sign=30aae2f2b6b3af2a6fc022e2a35ce538&ts=1682417669&user_id="
+    },
+    "__N_SSP": true
+  },
+  "page": "/episode/[id]",
+  "query": {
+    "id": "682b4b04457b22ce0d807186"
+  },
+  "buildId": "ukNZu6IpYNggYjnOGPzVW",
+  "assetPrefix": "",
+  "isFallback": false,
+  "gssp": true,
+  "customServer": true,
+  "scriptLoader": []
+}`;
+        
+        default:
+            return null;
+    }
+}
+
